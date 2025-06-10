e3b824c5863fb46e39975cecc8363accc457ac60
popsicleDeck <nguyenchuminh125@gmail.com>
2025-06-10 22:51:45 +1000

    change filename 'cuz modulename can't be started with number and has hyphens.

--- 2024/a1_1.py
+++ 2024/a1_1.py
@@ -1,5 +1,5 @@
 #handling input
-input_ = open("/home/chau/Projects/AdventOfCode/2024/1-1-input.txt", "r").read()
+input_ = open("/home/chau/Projects/AdventOfCode/2024/a1_1_input.txt", "r").read()
 input_ = input_.replace("\n", " ")
 input_ = input_.split(" ")
 input_ = [j for _,j in enumerate(input_) if j != '']
@@ -10,17 +10,19 @@ r_list = [j for i,j in enumerate(input_) if i % 2 != 0]
 l_list = list(map(int, l_list))
 r_list = list(map(int, r_list))
 
+#not needed for part 2
+
 #sorting
-l_list.sort()
-r_list.sort()
-
-input_ = list(zip(l_list, r_list))
-
-#calculation
-sum = 0
-for i in range(len(input_)):
-    sum += abs(input_[i][0] - input_[i][1])
-    i += 1
-print(sum)
+# l_list.sort()
+# r_list.sort()
+#
+# input_ = list(zip(l_list, r_list))
+#
+# #calculation
+# sum = 0
+# for i in range(len(input_)):
+#     sum += abs(input_[i][0] - input_[i][1])
+#     i += 1
+# print(sum)
 
 
---
+++ 2024/a1_2.py
@@ -0,0 +1,4 @@
+from a1_1 import l_list
+#checking for duplicates
+print(len(l_list))
+
